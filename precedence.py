69411dc7a44e7184ddd22fd31591216712f0e405
Samuel Simões <samuelsimoes31@gmail.com>
2020-07-17 15:55:15 -0300

    Precedence

---
+++ precedence.py
@@ -0,0 +1,60 @@
+# Highest precedence at top, lowest at bottom.
+# Operators on the same line evaluate left to right
+# https://api.sololearn.com/DownloadFile?id=3515
+# ()
+# Parentheses (grouping)
+
+# f(args...)
+# Function call
+
+# x[index:index]
+# Slicing
+
+# x[index]
+# Subscription
+
+# x.attribute
+# Attribute reference
+
+# **
+# Exponentiation
+
+# ~x
+# Bitwise not
+
+# +x, -x
+# Positive, negative
+
+# *, /, %
+# Multiplication, division, remainder
+
+# +, -
+# Addition, subtraction
+
+# <<, >>
+# Bitwise shifts
+
+# &
+# Bitwise AND
+
+# ^
+# Bitwise XOR
+
+# |
+# Bitwise OR
+
+# in, not in, is, is not, <, <=,  >,  >=,
+# <>, !=, ==	
+# Comparisons, membership, identity
+
+# not x
+# Boolean NOT
+
+# and
+# Boolean AND
+
+# or
+# Boolean OR
+
+# lambda
+# Lambda expression
