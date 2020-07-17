1c434dcb79574289030e1766b8b6ee916c9274e3
Samuel Simões <samuelsimoes31@gmail.com>
2020-07-17 15:53:24 -0300

    Imprecision

---
+++ if-and-imprecision.py
@@ -0,0 +1,33 @@
+print((3 * 0.1)  == 0.3)
+print((3 * .1)  == .3)
+print(float(3 * 0.1)  == float(0.3))
+print((3 * 0.1)  > 0.3)
+print((3 * 0.1)  < 0.3)
+
+if 10>5:
+    print('10 > 5')
+if 10>5:
+    print('OPA');print('IAE')
+
+num = 12
+if num > 5:
+   print("Bigger than 5")
+   if num <=47:
+      print("Between 5 and 47")
+
+# x = int(input("Enter a number: "))
+x=5
+if x == 5:
+    print("Yes")
+else:
+    if x < 5:
+        print("Less then 5")
+    else:
+        if x > 5:
+            print("Greater then 5")
+if x == 5:
+    print("Yes")
+elif x < 5:
+    print("Less then 5")
+else:
+    print("Greater then 5")
