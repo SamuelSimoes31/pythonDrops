c467c5a5d9ca65a786fd9b6d56c086367c041a0a
Samuel Simões <samuelsimoes31@gmail.com>
2020-07-17 15:50:08 -0300

    Hello World

---
+++ helloWorld.py
@@ -0,0 +1,107 @@
+# PRINTANDO
+print("Hello")
+print('Hello')
+print('Hello','IHU','OPA')
+print(1+2+3)
+print('1+2+3')
+print('1'+'2'+'3')
+ 
+#------------------------------------------------------------------------------------------
+ 
+print (2*(3+4))
+print(10/2) #python 3 return float, python 2 return integer
+print(10.0//2)
+print(10//2.0)
+print(10//2)
+print(9/2) 
+print(9//2)
+print(11/3)
+print(11//3)
+
+#------------------------------------------------------------------------------------------
+
+# print(11/0)
+# Traceback(most recent call last):
+# This line tells that it is going to return the sequence of Failures / functions called / commands executed in reverse order of actual call/execution.You need not get much into details of call stacks as of now but that's ,what it means overall.
+
+# File "<stdin>", line 1, in <module>
+# Every Call stack statement is composed of -
+# a) File Name(from where input is coming for execution)
+# b) Line No. executed
+# c) Module no
+# Here, since you are working on console itself, so the file name is set to default "standard input". Line no that was executed is 1. Module also set to default.
+
+# ZeroDivisionError
+# As described in comments, last line always gives the error produced while executing that Line No. Here, since division by 0 is the error, hence it is listed.
+ 
+#------------------------------------------------------------------------------------------
+ 
+print(2**5)
+print(2**2**2)
+print(9**(1/2))
+print(3%2)
+print(1.25 % 0.5)
+ 
+#------------------------------------------------------------------------------------------
+ 
+'Hello'
+# \v 	Line Tabulation
+# \u2028 	Line Separator (Unicode)
+print("""ORA
+VEJAM
+SÓ""")
+print(
+'''ORA
+VEJAM
+SÓ''')
+print ("Spam" , "in", "brain", sep='+', end='!\n')
+ 
+#------------------------------------------------------------------------------------------
+ 
+# s= input("Enter your name : ")
+# print(s)
+# k= input(" Enter your age : ")
+# print(k)
+# t= input(" About Yourself: \n")
+# print(t)
+print('First string' + ", " + "second string")
+# print(1 + '2' + 3 + '4')
+print(1 + int('2') + 3 + int('4'))
+print('baby shark',', doo'*6)
+print('baby shark',', doo'*0)
+# str + str OK
+# str * int OK
+# str + int WRONG
+# str * float WRONG
+ 
+#------------------------------------------------------------------------------------------
+ 
+# x = float(input("Enter a number: "))
+# print(x)
+# y = float(input("Enter another number: "))
+# print(y)
+# print(x + y)
+a, b, c = 6, 5, 8
+print(a)
+print(b, c)
+x = 123.456
+print(x)
+123.456789
+x = "This is a string"
+print(x + "!")
+ 
+#------------------------------------------------------------------------------------------
+ 
+print('RESERVADO:','False','class','finally','is','return','None','continue','for','lambda','try','True','def','from','non','local','while','and','del','global','not','with','as','elif','if','or','yield','assert','else','import','pass','break','except','in','raise')
+ 
+#------------------------------------------------------------------------------------------
+ 
+# i = input()
+# # print(i+2) # Num pode somar int com str
+# print(int(i)+2)
+ 
+#------------------------------------------------------------------------------------------
+ 
+#++ NUM EXISTEEEE ;-;
+
+quit() # pra quitaaaaaaaaaaaaaar
