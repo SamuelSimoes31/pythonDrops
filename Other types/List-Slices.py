befa9bb2a9c7939e72e390adcd36200d32b20c1b
Samuel Simões <samuelsimoes31@gmail.com>
2020-07-18 19:14:03 -0300

    List Slices

---
+++ Other types/List-Slices.py
@@ -0,0 +1,22 @@
+squares = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+print(squares[2:6])
+print(squares[3:8])
+print(squares[0:1])
+
+mylist = [1, 2, 3, 4]
+
+print(mylist[::-1])
+
+print(mylist[2:])
+print(mylist[:2])
+
+print(squares[::2])
+print(squares[2:8:3])
+
+print(mylist[1:-1])
+
+sqs = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+print(sqs[1::4])
+
+sqs = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+print(sqs[7:5:-1])
