8126b3c00f369f93c7c11d3b5d031c154864d68e
Samuel Simões <samuelsimoes31@gmail.com>
2020-07-18 18:22:46 -0300

    None

---
+++ Other types/none.py
@@ -0,0 +1,15 @@
+# none euivale a null em outras linguagens
+print(None)
+
+def some_func():
+  print("Hi!")
+
+var = some_func()
+print(var)
+
+foo = print()
+if foo == None:
+  print(1)
+else:
+  print(2)
+
